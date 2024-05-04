83731db4ff147d7eccc31bad7fa73eb6f5835aa3
Jonast96 <Jon.Astveit@hotmail.com>
2024-05-04 21:28:41 +0200

    feat: working api call

--- obj/Debug/net8.0/foodApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/foodApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a0b80698d59ac976ba221c26fa7f19c9a4cece")]
 [assembly: System.Reflection.AssemblyProductAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
