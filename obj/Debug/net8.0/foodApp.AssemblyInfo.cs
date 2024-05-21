b5ccc6ac053101265f96ede5a6e96e6ad9f5ec14
Jon <88549310+Jonast96@users.noreply.github.com>
2024-05-21 08:36:02 +0200

    n

--- obj/Debug/net8.0/foodApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/foodApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a48bdc14aea6c7144e12e565609ac9a79e1d13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0c85e90c18ba23a00d9272789889526c4f9be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
