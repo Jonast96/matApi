b4f84d8af097a3309f0df69e0b6cdfc73882e834
Jon <88549310+Jonast96@users.noreply.github.com>
2024-05-21 08:52:29 +0200

    feat: logic for history

--- obj/Debug/net8.0/foodApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/foodApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdaec0bb3f096b173253230f30a696a196642284")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f26bcd994a7ae05fcb89063d8b3dadb3874e9845")]
 [assembly: System.Reflection.AssemblyProductAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("foodApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
