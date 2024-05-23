9832ec01bd7fc78f62e453551b5c0f108a4caf9b
Jon <88549310+Jonast96@users.noreply.github.com>
2024-05-23 12:59:54 +0200

    feat: design for favorites

--- Models/PrevViewedModel.cs
+++ Models/PrevViewedModel.cs
@@ -3,4 +3,5 @@ public class PrevViewedModel
     public int Id { get; set; }
     public string Name { get; set; } = "";
     public string ImageUrl { get; set; } = "";
+    public double Price { get; set; } = 0;
 }
